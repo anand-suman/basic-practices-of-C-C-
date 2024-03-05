467c5f1108a08baec4a3bd0c29bdfe2a230b484d
Anand Kumar Suman <123244254+anand-suman@users.noreply.github.com>
2024-03-05 12:27:34 +0000

    funtion and stores 01

---
+++ basic/fun_01.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+
+int main()
+{
+    /*function apne aap me ek program hai like aise samjh skta hu,
+    like ek machine jisak ek special task karta hai
+    
+    fun(int);// compiler assuming that the return type of the funtion is integr bydefolt
+    int main()
+    {
+        int x; 
+        x=fun(10);
+        printf("%d", x);
+        return 0;
+
+        double fun(int y) // it will give the error bcz bydefolt compiler stor the funtion return type is integer but we difind the funtion return type in double
+        {
+            double temp= 12.43;
+            return temp*y;
+        }
+    }
+============================================ Srorage class =============================
+
+1.=> scope:- part of program/code in which a variable can be accessed (visibility of variable)
+
+
+2.=> Ligetime:- Duration (Active/alive)
+
+3.=> Degault value:- what is the value of variable id we don't initialize it.
+
+4.=> storage Area:- where a variable is stored
+
+
+
++++++++++++++++++++++++++++ AUTO +++++++++++++++++++++++++++++++++++++++++++++++++++
+       1.=> SCOPE :- "block in which they are declared".
+       2.=> LIGETIME:- "block in which they are declared"
+       3.=> DEGAULT VALUE:- "garbage"
+       4.=> STPRAGE AREA:- "stack" 
+=> main scope variables are accessible to sub-scope variables.
+=> sub-scope variables are not accessible to main scope variavbles.
+=> local/auto variables are created automatically wheb we eter the block in whivh they are declared and destructed automatrically when we exit the block.
+
+
+++++++++++++++++++++++++ REGISTER +++++++++++++++++++++++++++++++++++++++++++++++++++
+        **not in use, bahut problem hai baba idhar
+            kabhi idhar toh kabhi udahr ( kab iss ko register grant hua ya nhi pata anhi chalta);
+        **ye low level ke language me hota hai, high level me nhi
+        * As same as auto
+        * storage area : cpu register/ stack
+       1.=> SCOPE :- "block in which they are declared".
+       2.=> LIGETIME:- "block in which they are declared"
+       3.=> DEGAULT VALUE:- "garbage"
+       4.=> STPRAGE AREA:- "stack"        
+    
++++++++++++++++++++++++++++ STATIC +++++++++++++++++++++++++++++++++++++++++++++++++++
+       1.=> SCOPE :- "block in which they are declared".
+       2.=> LIGETIME:- "Program"
+       3.=> DEGAULT VALUE:- "0"
+       4.=> STPRAGE AREA:- "static area (date segnemet)" 
+=> The value persist between diggent funtion call.
+=> No redeclarations.
+=> They are created only once in the program.
+   
++++++++++++++++++++++++++++ GLOBAL(EXTERN) +++++++++++++++++++++++++++++++++++++++++++++++++++
+       1.=> SCOPE :- "though out the program".
+       2.=> LIGETIME:- "Throug out the program"
+       3.=> DEGAULT VALUE:- "0"
+       4.=> STPRAGE AREA:- "stack area" 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    */
+
+}
+
+// int mul(int x, int y)  run tiem error dega
+// {
+//     return x*y;
+// }

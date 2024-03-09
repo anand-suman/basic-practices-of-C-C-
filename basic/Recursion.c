591e380cf30cafe9695c0e7df5335e0cc0008841
Anand Kumar Suman <123244254+anand-suman@users.noreply.github.com>
2024-03-09 12:18:04 +0000

    recursion

--- basic/Recursion.c
+++ basic/Recursion.c
@@ -37,25 +37,101 @@ void fun(int n)
 
 
              trace this tree from top to bottom and left to right            
- 
+ => the arguments/value for which the fuction does not call itself => base value/ base argument.
 
 */
 
 #include<stdio.h>
- void fun(int n) 
-    {
+//  void fun(int n) 
+//     {
        
-        if(n<=0)
-        {
-            return;
-        }
-        else
-        {
-            fun(n-1);
-           printf("%d\n",n);// recersive call
-            fun(n-1);
-        }
+//         if(n<=0)
+//         {
+//             return;
+//         }
+//         else
+//         {
+//             fun(n-1);
+//            printf("%d\n",n);// recersive call
+//             fun(n-1);
+//         }
+//     }
+
+// int f2(int n)
+// {
+//     if(n<=1)
+//     {
+//         return n;
+//     }
+//     if(n%2)
+//     return f2(n/2)+n;//odd pe chale ga
+//     return f2(n/3)+n;// even pe chale ga
+// }
+
+
+// int f2(int n)
+// {
+//     if(n<=1)
+//     {
+//         return n;
+//     }
+//     return f2(n/2)+n/2;
+// }
+
+// unsigned int f2(unsigned int n, unsigned int r)
+// {
+//     if(n>0)
+//     {
+//         return (n%2)+f2(n/r,r);
+//     }
+//     else
+//     return 0;
+// }
+
+
+
+// int f2(int n)
+// {
+//     if(n%2==0)
+//     {
+//         return f2(f2(n-1));
+//     }
+//     return (n++);
+// }
+
+
+// int f2(int n)
+// {
+//     if(n<=1)
+//     {
+//         return n;
+//     }
+//     return f2(n/2)+f2(n/2)+1;
+// }
+// int f2(int n)
+// {
+//     if(n>3)
+//     {
+//         return f2(n-4)+f2(n-1)+1;
+//     }
+//     return 1;
+//     ;
+// }
+
+int f2(unsigned int n)
+{
+    if(n==0 || n==1)
+    {
+        return n;
     }
+    if(n%3!=0)
+    {
+        return 0;
+    }
+    return f2(n/3);
+}
+
+
 //  int power(int a,int b)
 //     {
        
@@ -68,6 +144,10 @@ void fun(int n)
 //             return (a*power(a,b-1));// recersive call
 //         }
 //     }
+
+
+
+
 //  void print(int n)
 //     {
 //         if(n==1)
@@ -80,6 +160,10 @@ void fun(int n)
 //             print(n-1);// recersive call
 //         }
 //     }
+
+
+
+
 int main()
 {
     //exam 01
@@ -88,7 +172,32 @@ int main()
    //exam 02
     // n=power(5,4);
     // printf("%d", n);
-    fun(3);
+    // fun(3);
+    // printf("\n %d \n", f2(12)); 
+    printf("\n %d \n", f2(3));  
+    // printf("\n %d \n", f2(12));  
+    // printf("\n %d \n", f2(513,2)); 
 
     return 0;
 }
+
+// void main()
+// {
+//     static int var =5;
+//     //var=var-1;
+//     if(--var)
+//     {
+//         printf("%d",var);
+//         main();
+//         printf("%d",var);
+    
+//     }    
+// }
+
+// void main()
+// {
+//     static int var =5;
+//     printf("%d",var--);
+//     if(var)
+//     main();
+// }

f9f5620ec6eabacdd85cf3b6f6f4c60a3e150ae9
Anand Kumar Suman <123244254+anand-suman@users.noreply.github.com>
2024-03-16 18:18:34 +0000

    Array

--- Array.c
+++ Array.c
@@ -38,7 +38,7 @@
     => address + value = address
     => value + address = address
     => address + address = not addres not valid
-    
+
 ======================================================================================================================================
 
 => if the declarationod an array has n-dimensions then
@@ -46,29 +46,85 @@
                                             b) if we be give the less then n-dimaention => address
 
          
+*(a+1)=a[1] <=>  *(1+a)
 
+*(a+2)=a[2]
 
+*(a+i)=a[i] <=> *(i+a)
+a[i]<=> i[a]
 
 
 
 */
 
 
+// int main()
+// {
+//     // int a;// 1 variable
+//     // int b[4];// collection of multiple variable , sab me garbage value hoga
+//     // int c[4]={3,4};//collection of 4 variable but 2 place pe value or baki ke place pe 0 asign ho jaye ga
+//     //int d[-2];// invalid or inliagal
+//     //int d[0];// invalid or inliagal
+    
+//     // printf("%d",a);// garbage
+//     // printf("%d",b[0]);// garbage
+
+//     // printf("%u",a);// addres of 1st element 
+//     // printf("%u",&a[0]);// addres of 1st element 
+//                 // Numarical  value same but first => adress of an element of 4bype 
+//                                             //second => "    "  "     "    of 16 Byte
+//     // printf("%u",&a);
+
+//     // int a[4]={10,20,30,40};
+//     int a[5]={10,20,30,40,50};
+//     // printf("%d\n",a[2]);
+//     // printf("%d",*(a+2));
+
+//     // printf("%d\n",a[1]);
+//     // printf("%d\n",*(a+1));
+//     // printf("%d\n",1[a]);
+//     // printf("%d\n",*(1+a));
+
+
+
+//     // printf("%u\n",a);//addres of 1st elements
+//     // printf("%u\n",&a);//address of whole addres
+//     // printf("%u\n",a+1);// address of of 2nd element
+//     // printf("%u\n",&a+1);//Adddress of entire array(20 bytes)
+
+//     return 0;
+// }
+
+//=============================================================== 2D ARRAY =============================================================================
+
+/*
+ *(a[0]+1) = a[0][1]
+ *(a[0]+2) = a[0][2]
+ *(a[0]+j) = a[0][j]
+
+
+ *(a[i]+j) = a[j][j] <=> *(*(a+i)+j )
+
+*/
+
+
+
+
 int main()
 {
-    int a;// 1 variable
-    int b[4];// collection of multiple variable , sab me garbage value hoga
-    int c[4]={3,4};//collection of 4 variable but 2 place pe value or baki ke place pe 0 asign ho jaye ga
-    //int d[-2];// invalid or inliagal
-    //int d[0];// invalid or inliagal
-    
-    // printf("%d",a);// garbage
-    // printf("%d",b[0]);// garbage
+ int a[2][3]={1,2,3,4,5,6,};
 
-    // printf("%u",a);// addres of 1st element 
-    // printf("%u",&a[0]);// addres of 1st element 
-                // Numarical  value same but first => adress of an element of 4bype 
-                                            //second => "    "  "     "    of 16 Byte
-    // printf("%u",&a);
-return 0;
+//  printf("%u\n",a);
+//  printf("%u\n",a[0]);  //sab same hai
+//  printf("%u\n",&a);
+
+
+//  printf("%u\n",a+1);
+//  printf("%u\n",a[0]+1);
+//  printf("%u\n",&a+1);
+
+
+
+
+    return 0;
 }

648834ca06593b77bfead68ba944eaa3159efcdd
Anand Kumar Suman <123244254+anand-suman@users.noreply.github.com>
2024-03-28 12:18:22 +0000

    string

---
+++ basic/string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*
+    sequence of charecter terminated by NULL character
+
+
+
+
+
+
+*/
+
+
+
+int main()
+{
+    char arr[10] = "Anand"
+    printf("%s",arr);
+
+    return 0;
+}
